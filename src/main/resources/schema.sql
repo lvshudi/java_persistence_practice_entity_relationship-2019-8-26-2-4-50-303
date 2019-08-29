f80806d0f8b5f9690280cf57df973f461982a6cb
lvshudi <lvshudi119@126.com>
2019-08-29 19:45:56 +0800

    finish

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,56 @@ CREATE TABLE employee (
   name VARCHAR(64) NOT NULL,
   age   int(4) NOT NULL
 );
+
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`System_Administrator`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`System_Administrator` (
+  `userID` INT NOT NULL,
+  `userName` VARCHAR(45) NOT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`userID`))
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Company`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Company` (
+  `CompanyID` INT NOT NULL,
+  `CompanyAdminID` VARCHAR(45) NOT NULL,
+  `CompanyName` VARCHAR(45) NOT NULL,
+  `userID` INT NOT NULL,
+  PRIMARY KEY (`CompanyID`),
+  CONSTRAINT `SysAdMinID`
+    FOREIGN KEY (`userID`)
+    REFERENCES `mydb`.`System_Administrator` (`userID`)
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`CompanyAdministrator`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`CompanyAdministrator` (
+  `CompanyAdminID` INT NOT NULL,
+  `CompanyAdminName` VARCHAR(45) NOT NULL,
+  `EmployeeID` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`CompanyAdminID`),
+  CONSTRAINT `company`
+    FOREIGN KEY (`CompanyAdminID`)
+    REFERENCES `mydb`.`Company` (`CompanyID`)
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`Employee`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`Employee` (
+  `EmployeeID` INT NOT NULL,
+  `CompanyID` INT NOT NULL,
+  PRIMARY KEY (`EmployeeID`, `CompanyID`),
+  CONSTRAINT `CompanyAdmID`
+    FOREIGN KEY (`EmployeeID`)
+    REFERENCES `CompanyAdministrator` (`CompanyAdminID`)
+  CONSTRAINT `CompanyID`
+    FOREIGN KEY (`CompanyID`)
+    REFERENCES `mydb`.`Company` (`CompanyID`)
